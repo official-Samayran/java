dda7f8b3c2fad12a2fcc1bf3788cbe724ad84315
Official-Samayran <officialsamayran@gmail.om>
2025-08-08 14:50:33 +0530

    Comments

---
+++ comments/comments.java
@@ -0,0 +1,12 @@
+package comments;
+
+public class comments{
+    public static void main(String[] args) {
+        // This is a Comment
+        System.out.println("Comments are ignored by compiler.");
+        /*
+         * This is a MultiLine Comment.
+         * We can use it to Comment multiple lines and blocks.
+         */
+    }
+}

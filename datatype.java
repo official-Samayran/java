96f89a9110eb3957f11c2def85567a0488265b14
Official-Samayran <officialsamayran@gmail.om>
2025-08-08 13:55:49 +0530

    defining Datatypes

---
+++ datatype.java
@@ -0,0 +1,13 @@
+public class datatype {
+    public static void main(String[] args) {
+        int number = 1;
+        double decimal = 19.3;
+        char character = 'S';
+        boolean isDone = true;
+
+        System.out.println("Number - " + number);
+        System.out.println("Decimal - " + decimal);
+        System.out.println("Character - " + character);
+        System.out.println("is Done - " + isDone);
+    }
+}

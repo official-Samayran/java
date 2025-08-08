499c02691fb3ed46258760b51ac334039718bd17
Official-Samayran <officialsamayran@gmail.om>
2025-08-08 14:41:46 +0530

    constants

---
+++ constants/constants.java
@@ -0,0 +1,9 @@
+package constants;
+
+public class constants {
+    public static void main(String[] args){
+        final double pi = 3.14159;
+        System.out.println("Pi = " + pi);
+    }
+    
+}

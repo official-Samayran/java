93799bf9baeaf3d4adcdc565f91818cac61c17f1
Official-Samayran <officialsamayran@gmail.om>
2025-08-08 14:17:41 +0530

    Typecasting

---
+++ Type_Casting/typecasting.java
@@ -0,0 +1,11 @@
+public class typecasting{
+    public static void main(String[] args) {
+        int myInt = 7;
+        double myDouble = myInt;
+        System.out.println("Int to Double - " + myDouble);
+
+        double myDouble2 = 12.6;
+        int myInt2 = (int) myDouble2;
+        System.out.println("Double to Int - " + myInt2);
+    }
+}

e810a1bcbfa055fb8a0ea1076f0800e20c91715a
Official-Samayran <officialsamayran@gmail.om>
2025-08-11 23:53:46 +0530

    Operators

---
+++ operators/operators.java
@@ -0,0 +1,18 @@
+package operators;
+public class operators {
+    public static void main(String[] args){
+
+        // + - * / %
+        int a = 10, b = 5;
+        
+        System.out.println("A = 10 | B = 5");
+        System.out.println("A + B = " + (a+b));
+        System.out.println("A - B = " + (a-b));
+        System.out.println("A * B = " + (a*b));
+        System.out.println("A / B = " + (a/b));
+        System.out.println("A % B = " + (a%b));
+        
+        
+
+    }    
+}

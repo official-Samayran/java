1dbd794919c69f7c0feb62356f06212748d494f9
Official-Samayran <officialsamayran@gmail.om>
2025-08-17 14:19:18 +0530

    added comments in example1.java

--- methods/example1.java
+++ methods/example1.java
@@ -2,14 +2,17 @@ package methods;
 
 public class example1 {
 
+    //Defining new method Named "samayran"
     public static void samayran() {
         System.out.println("My name is Samayran");
     }
 
+    //Defining new method named "java"
     public static void java() {
         System.out.println("This code is written in JAVA language.");
     }
 
+    //calling both methods in main method
     public static void main (String[] args) {
         samayran();
         java();

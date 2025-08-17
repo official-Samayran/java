794c5b0056592ee9747b29da15717ceff691d689
Official-Samayran <officialsamayran@gmail.om>
2025-08-17 14:17:06 +0530

    Methods > example1.java

---
+++ methods/example1.java
@@ -0,0 +1,17 @@
+package methods;
+
+public class example1 {
+
+    public static void samayran() {
+        System.out.println("My name is Samayran");
+    }
+
+    public static void java() {
+        System.out.println("This code is written in JAVA language.");
+    }
+
+    public static void main (String[] args) {
+        samayran();
+        java();
+    }
+}

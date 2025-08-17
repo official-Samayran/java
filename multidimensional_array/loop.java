74ab9591e1fbe54977317cfe00ecd93d438a13e3
Official-Samayran <officialsamayran@gmail.om>
2025-08-17 16:11:48 +0530

    Loops in 1D and 2D Array

---
+++ multidimensional_array/loop.java
@@ -0,0 +1,23 @@
+package multidimensional_array;
+public class loop {
+    
+    public static void array() {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        for(int i=0 ; i<matrix.length; i++){
+            for(int j=0 ; j<matrix[i].length ; j++) {
+                System.out.println(matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+    
+    
+    public static void main(String[] args) {
+        array();
+    }
+}

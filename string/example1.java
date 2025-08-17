9caa5808352394a45be751619cfacd191ac59f1a
Official-Samayran <officialsamayran@gmail.om>
2025-08-17 20:14:30 +0530

    Strings

---
+++ string/example1.java
@@ -0,0 +1,15 @@
+package string;
+
+public class example1 {
+    public static void main(String[] args){
+        String name = "Samayran";
+        String greet = "Hello";
+
+        System.out.println(greet + " " + name + "!");
+        System.out.println("Length of name - " + name.length());
+        System.out.println("Character at 5 - " + name.charAt(5));
+        System.out.println("Uppercase Name - " + name.toUpperCase());
+        System.out.println("Lowercase Name - " + name.toLowerCase());
+
+    }
+}

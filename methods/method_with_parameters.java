b4957fdcf7177e79cea9b83f32745aa87bf6f15b
Official-Samayran <officialsamayran@gmail.om>
2025-08-17 14:28:30 +0530

    Method with Parameters

---
+++ methods/method_with_parameters.java
@@ -0,0 +1,11 @@
+package methods;
+
+public class method_with_parameters {
+    public static  void main (String[] args) {
+        greetUser("Samayran");
+    }
+
+    public static void greetUser(String name) {
+        System.out.println("Hello " + name + " !");
+    }
+}

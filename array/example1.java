8356adad487ef159010c811716e9766ae7cf7b0a
Official-Samayran <officialsamayran@gmail.om>
2025-08-17 15:18:28 +0530

    Arrays

---
+++ array/example1.java
@@ -0,0 +1,18 @@
+
+
+public class example1 {
+    public static void main(String[] args) {
+        array();
+    }
+
+    public static void array() {
+        int[] num1 = {12, 23, 42, 1, 5};
+        System.out.println(num1[0]);
+        
+        int[] num2 = new int[3];
+        num2[0] = 10;
+        num2[1] = 20;
+        num2[2] = 30;
+        System.out.println(num2[2]);
+    }
+}
